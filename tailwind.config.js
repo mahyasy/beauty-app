6c7ae266c5bd950a369e3eb35e214398d1e9ac98
mahyasy <mahyayousefii@yahoo.com>
2024-06-14 16:28:02 +0330

    service component added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,19 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+   screens:{
+    "xs":{'max':'414px'},
+    'sm': '640px',
+    'md': '768px',
+  
+
+   },
     colors:{
       "brown":"#655F5F",
       "pink": "#E1999F",
       "cream": "#FFF9E7",
-      "gray": "#D3D3D3"
+      "gray": "#D3D3D3",
+      "white": "#ffffff"
      
     },
     extend: {
