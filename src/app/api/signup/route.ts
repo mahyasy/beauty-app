40cc549719564be8eda41c9a8692a113a4b2ca05
Alireza Asghari <aa9967316@gmail.com>
2024-06-20 10:47:07 +0330

    🏷️ Update type

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 
 import { NextResponse } from "next/server";
 import connectDB from "../../../utils/connectDB";
