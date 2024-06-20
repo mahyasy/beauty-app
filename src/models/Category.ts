42badcd21687f1b61aa6de675f5f83967c9e269b
Alireza Asghari <aa9967316@gmail.com>
2024-06-20 10:45:53 +0330

    🗃️ Create category model

---
+++ src/models/Category.ts
@@ -0,0 +1,18 @@
+import { model, models, Schema } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    faName: { type: String, required: true },
+    images: [{type: String}],
+    parentCategory: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = models.Category || model("Category", categorySchema);
+export default Category;
