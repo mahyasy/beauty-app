3251473a677023294068243e6d82adfd20a7ef9e
mahyasy <mahyayousefii@yahoo.com>
2024-06-30 15:00:44 +0330

    font changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,8 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily:{
-        cur:['fangsong;']
+        cur:['fangsong;'],
+        amiri:['"Amiri", serif']
 
       },
       fontSize :{
