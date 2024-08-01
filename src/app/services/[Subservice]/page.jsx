a68735dd3d25e1b9c30fade7cd7c19629f92618c
mahyasy <mahyayousefii@yahoo.com>
2024-08-01 19:27:28 +0330

    sub service

---
+++ src/app/services/[Subservice]/page.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+    <div className='flex flex-wrap justify-around mt-24 items-center'>
+        <section className='bg-pink max-w-24 max-h-24 text-white drop-shadow-5xl p-10 rounded-lg'>
+         موقت
+        </section>
+        <section className='bg-pink text-center max-w-24 max-h-24 text-white drop-shadow-5xl p-10 rounded-lg'>
+         اکستنشن مزه
+        </section>
+        
+        
+
+        <div className='flex justify-around items-center '>
+  
+    <figure className='hidden md:block '>
+    <Image
+            className="rounded-t-full drop-shadow-5xl h-auto  md:w-[150px] "
+            src="/seven.jpg"
+            width="105"
+            priority={false}
+            height="0"
+            alt="shabgis"
+          />
+            <aside className="flex flex-col items-center">
+          <h1 className="text-brown text-3xl font-cur relative left-4 font-extrabold tracking-[.20em] text-center my-2 md:text-[30px]  ">
+            SHABGIS
+          </h1>
+          <h1 className="text-pink  font-bold mb-2">سالن زیبایی در لواسان</h1>
+          <p className="text-brown">زیبایی را از ما بخواهید</p>
+        </aside>
+    </figure>
+  
+
+ </div>
+        
+        
+
+    </div>
+  )
+}
+
+export default page
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,47 +5,43 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-   screens:{
-    "xs":{'max':'414px'},
-    'sm': '640px',
-    'md': '768px',
-  
 
-   },
-    colors:{
-      "brown":"#655F5F",
-      "pink": "#E1999F",
-      "cream": "#FFF9E7",
-      "gray": "#D3D3D3",
-      "white": "#ffffff",
-      "red": "#ff0000",
-      "gray": "#D3D3D3"
-     
+  theme: {
+    screens: {
+      xs: { max: "414px" },
+      sm: "640px",
+      md: "768px",
+    },
+    colors: {
+      brown: "#655F5F",
+      pink: "#E1999F",
+      cream: "#FFF9E7",
+      gray: "#D3D3D3",
+      white: "#ffffff",
+      red: "#ff0000",
+      green: "#4FFFB0",
     },
     extend: {
-
       dropShadow: {
-        '3xl': '3px 3px 1px rgba(0, 0, 0, 0.30)',
-        '4xl': [
-            '0 35px 35px rgba(0, 0, 0, 0.25)',
-            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        "3xl": "3px 3px 1px rgba(0, 0, 0, 0.30)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
         ],
-        '5xl':'7px 7px 1px  rgba(0,0,0,0.25)',
+        "5xl": "7px 7px 1px  rgba(0,0,0,0.25)",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily:{
-        cur:['fangsong;'],
-        amiri:['"Amiri", serif']
-
+      fontFamily: {
+        cur: ["fangsong;"],
+        amiri: ['"Amiri", serif'],
       },
-      fontSize :{
-        '2xl': '2rem',
-      }
+      fontSize: {
+        "2xl": "2rem",
+      },
     },
   },
   plugins: [],
