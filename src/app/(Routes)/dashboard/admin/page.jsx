f9725361c4dd2962d84c13414bea9d0c526bcb45
Alireza Asghari <aa9967316@gmail.com>
2024-08-09 19:08:03 +0330

    🗃️ Filter category data

--- src/app/(Routes)/dashboard/admin/page.jsx
+++ src/app/(Routes)/dashboard/admin/page.jsx
@@ -3,17 +3,17 @@ import Categories from "./Categories";
 import connectDB from "@/utils/connectDB";
 import Category from "@/models/Category";
 import FormSection from "./FormSection";
-import { Interface } from "readline";
 
 
 
 export default async function Admin() {
   await connectDB();
   const categories = await Category.find();
+  const filtered = categories.filter(item => !item.parentCategory)
 
   return (
     <div>
-      <FormSection categories={JSON.parse(JSON.stringify(categories))} />
+      <FormSection categories={JSON.parse(JSON.stringify(filtered))} />
       <div className="w-1/2">
         <Categories service={JSON.parse(JSON.stringify(categories))} />
       </div>
