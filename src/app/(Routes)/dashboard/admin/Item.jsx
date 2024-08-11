b3816fc3b331b5bab36cb4547e86a2f9c07a65b7
Alireza Asghari <aa9967316@gmail.com>
2024-08-11 17:19:08 +0330

    🩹 Fix nothing

--- src/app/(Routes)/dashboard/admin/Item.jsx
+++ src/app/(Routes)/dashboard/admin/Item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const Item = ({ item, setService }) => {
+const Item = ({ item }) => {
   return (
     <section
       key={item.id}
