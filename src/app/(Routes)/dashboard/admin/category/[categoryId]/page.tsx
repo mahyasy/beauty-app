f264e6ff15d004cd6188edd457ebf20e084d1450
Alireza Asghari <aa9967316@gmail.com>
2024-08-11 17:19:40 +0330

    ✨ Create category details page

--- src/app/(Routes)/dashboard/admin/category/[categoryId]/page.tsx
+++ src/app/(Routes)/dashboard/admin/category/[categoryId]/page.tsx
@@ -1,6 +1,71 @@
-const Categorydetials = ({ params }) => {
-  console.log(params);
-  return <div>catgeory - {params.categoryId}</div>;
+import { fetchCategoty } from "@/actions/actions";
+
+import Item from "../../Item";
+import Category, {
+  CategoryBySubServiceType,
+  CategoryType,
+} from "@/models/Category";
+import connectDB from "@/utils/connectDB";
+
+interface PropsInterface {
+  params: { categoryId: string };
+}
+
+const Categorydetials = async ({ params }: PropsInterface) => {
+  await connectDB();
+  const category = await Category.findById(params.categoryId);
+  if (!category) return <h1>دسته بندی مورد نظر یافت نشد</h1>;
+
+  const bySubServices: CategoryBySubServiceType[] | null =
+    await Category.aggregate([
+      { $match: { _id: category?._id } },
+      {
+        $lookup: {
+          from: "services",
+          foreignField: "category",
+          localField: "_id",
+          as: "subServices",
+        },
+      },
+    ]);
+
+  const { data, error }: { data?: CategoryType[]; error?: string } =
+    await fetchCategoty(params.categoryId);
+
+  if (error) return <h5>مشکلی در سرور رخ داد</h5>;
+  return (
+    <div className="p-4 px-10">
+      {category ? (
+        <div>
+          <h4 className="font-bold text-xl">
+            نام دسته بندی: {category.faName}
+          </h4>
+        </div>
+      ) : (
+        <span>در گرفتن اطلاعات دسته بندی مشکلی پیش امد</span>
+      )}
+
+      <div className="my-10">
+        <h5 className="text-xl">زیر دسته :</h5>
+        <div className="flex flex-wrap ">
+          {data.length === 0 && <h5>زیر دسته ای موجود نیست</h5>}
+          {data.map((item: CategoryType) => (
+            <Item key={item._id as string} item={item} />
+          ))}
+        </div>
+
+        <h5 className="text-xl">سرویس ها :</h5>
+        <div className="flex flex-wrap ">
+          {bySubServices[0].subServices.length === 0 && (
+            <h5>سرویسی در این دسته بندی موجود نیست</h5>
+          )}
+          {bySubServices[0].subServices.map((item) => (
+            <Item key={item._id as string} item={item} /> //change for item service
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Categorydetials;
