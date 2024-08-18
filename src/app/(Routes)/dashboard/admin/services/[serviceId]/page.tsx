5af86d2fc95970e57819313e91bad87fb465dff6
Alireza Asghari <aa9967316@gmail.com>
2024-08-18 19:14:24 +0330

    💄 Create card and details page for service

---
+++ src/app/(Routes)/dashboard/admin/services/[serviceId]/page.tsx
@@ -0,0 +1,33 @@
+import { fetchCategoty } from "@/actions/actions";
+
+import Item from "../../Item";
+import Category, {
+  CategoryBySubServiceType,
+  CategoryType,
+} from "@/models/Category";
+import connectDB from "@/utils/connectDB";
+import Service from "@/models/Service";
+
+interface PropsInterface {
+  params: { serviceId: string };
+}
+
+const Servicedetials = async ({ params }: PropsInterface) => {
+  await connectDB();
+  const service = await Service.findById(params.serviceId);
+  if (!service) return <h1>سرویس مورد نظر یافت نشد</h1>;
+
+  return (
+    <div className="p-4 px-10">
+      {service ? (
+        <div>
+          <h4 className="font-bold text-xl">نام سرویس: {service.faName}</h4>
+        </div>
+      ) : (
+        <span>در گرفتن اطلاعات سرویس مشکلی پیش امد</span>
+      )}
+    </div>
+  );
+};
+
+export default Servicedetials;
