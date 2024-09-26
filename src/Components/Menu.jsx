a8578a22715629f12e712a82d373b46fe21d1955
Alireza Asghari <aa9967316@gmail.com>
2024-09-26 10:38:04 +0330

    🔀 Merge fix

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -5,49 +5,51 @@ import { FiMenu } from "react-icons/fi";
 import { FcInfo } from "react-icons/fc";
 import { FcHome } from "react-icons/fc";
 import { HiScissors } from "react-icons/hi2";
-import {useOnClickOutside} from '../Components/Function/Function'
+import { useOnClickOutside } from "./Function/Function";
 import Link from "next/link";
 
 const Menu = () => {
   const navLink = [
-    {id:1, name: 'خانه', path:'/',tag:<FcHome/>},
-    {id:2, name: 'خدمات', path:'/services',tag:<HiScissors/>},
-    {id:3, name: 'درباره ما', path:'/about',tag:<FcInfo/>},
-    {id:4, name:"تماس با ما", path:'/call',tag:<FcPhone/>}
-
-  ]
+    { id: 1, name: "خانه", path: "/", tag: <FcHome /> },
+    { id: 2, name: "خدمات", path: "/services", tag: <HiScissors /> },
+    { id: 3, name: "درباره ما", path: "/about", tag: <FcInfo /> },
+    { id: 4, name: "تماس با ما", path: "/call", tag: <FcPhone /> },
+  ];
   const [isOpen, setOpen] = useState(false);
   const ref = useRef();
 
   const TogglerHandler = () => {
-    setOpen(!isOpen)
-    }
-   
-  
-  useOnClickOutside(ref,()=>setOpen(false))
+    setOpen(!isOpen);
+  };
+
+  useOnClickOutside(ref, () => setOpen(false));
 
   if (isOpen) document.documentElement.scroll = "disable";
 
   return (
-    <div >
+    <div>
       <FiMenu
         onClick={TogglerHandler}
         className="text-brown drop-shadow-3xl  "
       />
       {isOpen && (
-        <nav  ref={ref} className="absolute flex flex-col justify-around   border border-brown/20  right-7 shadow-lg top-10 w-40 h-40 bg-cream z-50 rounded-lg">
-
-  {navLink.map((link)=>{
-    return(
-   
-   <Link onClick={TogglerHandler} key={link.id}  className="flex flex-row mr-5 gap-2  items-center  text-brown "  href={link.path}>
-    <p className="text-sm">{link.name}</p>
-   <span className="text-lg">{link.tag}</span>
-   </Link>
-   
-
-  )})}
-       
+        <nav
+          ref={ref}
+          className="absolute flex flex-col justify-around   border border-brown/20  right-7 shadow-lg top-10 w-40 h-40 bg-cream z-50 rounded-lg"
+        >
+          {navLink.map((link) => {
+            return (
+              <Link
+                onClick={TogglerHandler}
+                key={link.id}
+                className="flex flex-row mr-5 gap-2  items-center  text-brown "
+                href={link.path}
+              >
+                <p className="text-sm">{link.name}</p>
+                <span className="text-lg">{link.tag}</span>
+              </Link>
+            );
+          })}
         </nav>
       )}
     </div>
