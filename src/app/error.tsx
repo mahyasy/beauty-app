943239667fee04239b2d8fe26e67792337a6802b
Alireza Asghari <aa9967316@gmail.com>
2024-09-26 13:26:53 +0330

    🥅 Error page

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+"use client"
+const Error = () => {
+  return (
+    <div className="w-full mt-10 flex justify-center items-center">
+      مشکلی در سرور رخ داد
+    </div>
+  );
+};
+
+export default Error;
